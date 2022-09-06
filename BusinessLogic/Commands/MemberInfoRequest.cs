4f3ed58671e404902e9e6199f5a5aadb568e5006
FedeMontenegro <fedejulianmontenegro@gmail.com>
2022-09-06 19:30:46 -0300

    Se agregó un bloque try catch para personalizar el mensaje de error en el método Execute del objeto MemberInfoRequest.

--- BusinessLogic/Commands/MemberInfoRequest.cs
+++ BusinessLogic/Commands/MemberInfoRequest.cs
@@ -9,14 +9,22 @@ namespace BusinessLogic.Commands
     {
         public void Execute()
         {
-            MemberInfoView view = new MemberInfoView();
-            InputData data = view.RequestData();
-            Member entity = new Member();
-            entity.ID = int.Parse(data.fields["CodigoSocio"]);
+            try
+            {
+                MemberInfoView view = new MemberInfoView();
+                InputData data = view.RequestData();
+                Member entity = new Member();
+                entity.ID = int.Parse(data.fields["CodigoSocio"]);
 
-            Member member = new MemberRepository().GetMember(entity.ID);
-            
-            view.ShowResult(member.FirstName + " " + member.SecondName);
+                Member member = new MemberRepository().GetMember(entity.ID);
+
+                view.ShowResult(member.FirstName + " " + member.SecondName);
+
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(string.Format("El código ingresado no existe en el sistema."));
+            }
         }
     }
 }
